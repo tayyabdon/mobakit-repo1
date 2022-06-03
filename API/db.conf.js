3d47fff830846b959c6cc6ed1f707e55770247d7
tayyabdon <m.raza.tayyab@vizteck.com>
2022-06-03 11:25:53 +0500

    pipeline configuration

--- API/db.conf.js
+++ API/db.conf.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const conn = mysql.createConnection({
-    host: 'test-project-database.cnq8lntbfse3.us-east-1.rds.amazonaws.com',
-    user: 'admin',
+    host: 'ath-database-instance-1.cz5ywjpgig5v.me-south-1.rds.amazonaws.com',
+    user: 'athadmin',
     port : 3306,
-    password: 'testProjectPassword',
-    database: 'test_project_database',
+    password: 'ATH#GADLANG',
+    database: 'test-project-db',
     insecureAuth : true
 });
 
---
+++ API/scripts/before_install.sh
@@ -0,0 +1,13 @@
+sudo apt install curl 
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+source ~/.profile   
+nvm install node 
+
+#create our working directory if it doesnt exist
+DIR="/home/ubuntu/mobakit-repo123/API"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi

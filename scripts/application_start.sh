f1f9f46a76e53eb3194773bd2552875fde610f3b
tayyabdon <m.raza.tayyab@vizteck.com>
2022-06-03 17:43:33 +0500

    logs

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -10,7 +10,7 @@ cd /home/ec2-user/mobakit-repo123/API
 
 sudo apt-get install npm
 
-#add npm and node to path
+#add npm and node to path 
 export NVM_DIR="$HOME/.nvm"	
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)

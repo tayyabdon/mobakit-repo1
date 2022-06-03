67a3f67657decfc9a7dc85e0e84108ad0b11cc82
tayyabdon <m.raza.tayyab@vizteck.com>
2022-06-03 16:21:56 +0500

    logs

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ubuntu/mobakit-repo123
+sudo chmod -R 777 /home/ec2-user/mobakit-repo123
 
 #navigate into our working directory where we have all our github files
-cd /home/ubuntu/mobakit-repo123/API
+cd /home/ec2-user/mobakit-repo123/API
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-#install node modules
+#install node modules 
 npm install
 
 #start our node app in the background

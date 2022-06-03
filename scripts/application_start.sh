c329166d8e3a496c157215ae107d400544a6f705
tayyabdon <m.raza.tayyab@vizteck.com>
2022-06-03 18:22:51 +0500

    logs

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -10,16 +10,15 @@ cd /home/ec2-user/mobakit-repo123/API
 
 # sudo apt-get install npm
 
-#add npm and node to path 
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads
 
 #install node  modules 
 npm install 
 
 npm install pm2 -g
-
+ 
 pm2 start index.js --name server
 
 #start our node app in  the background 

fc3e6bd5129c66147a64a1efd81c4e85421f0662
tayyabdon <m.raza.tayyab@vizteck.com>
2022-06-03 16:05:10 +0500

    pipeline configuration

--- API/scripts/application_start.sh
+++ API/scripts/application_start.sh
@@ -11,7 +11,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-#install node modules
+#install node modules 
 npm install
 
 #start our node app in the background

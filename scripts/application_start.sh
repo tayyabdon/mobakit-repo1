a3716c50beb4e2a253be4b9214c8f2aa200d74a5
Asif <asyfsaeed@gmail.com>
2022-06-04 15:57:10 +0500

    lets do it

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
-
-# source /home/ec2-user/.bash_profile
-
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/mobakit-repo123
-
-#navigate into our working directory where we have all our github files
-cd /home/ec2-user/mobakit-repo123/API
-
-# sudo apt-get install npm
-
-#!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-export HOME="/home/ec2-user/"
-sudo PM2_HOME=/home/ubuntu/.pm2 pm2 list
+set -e
 cd /home/ec2-user/mobakit-repo123
 npm install
-
 npm install pm2 -g
- 
-pm2 start index.js --name server
-
-#start our node app in the background  
-# node index.js > app.out.log 2> app.err.log < /dev/null &
-# node index.js  
+pm2 start app.js --name one
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-
-# source /home/ec2-user/.bash_profile
-
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-
-#create our working directory if it doesnt exist
-DIR="/home/ec2-user/mobakit-repo123" 
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
-
-# [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh  # This loads NVM
+set -e
+yum update -y
+pm2 update
